7bc0d95b4c1d86f45e3a3551a231789fc1be74c6
Konrad Guzek <konrad@guzek.uk>
2025-04-03 11:30:38 +0200

    dsaa(lab5): init lab 5

---
+++ aisd/dsaa.lab05/Document.java
@@ -0,0 +1,127 @@
+package dsaa.lab05;
+
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Document {
+    public String name;
+    public TwoWayCycledOrderedListWithSentinel<Link> link;
+    private static final String LINK_PREFIX = "link=";
+    public static final Pattern LINK_ID_PATTERN = Pattern.compile("^[a-zA-Z][a-zA-Z0-9_]*$");
+    public static final Pattern LINK_PATTERN = Pattern.compile("^([a-zA-Z][a-zA-Z0-9_]*)(?:\\(([0-9]+)\\))?$");
+
+    public Document(String name, Scanner scan) {
+        this.name = name.toLowerCase();
+        link = new TwoWayCycledOrderedListWithSentinel<>();
+        load(scan);
+    }
+
+    @SuppressWarnings("DuplicatedCode")
+    public void load(Scanner scan) {
+        String line = "";
+        while (!line.equals("eod")) {
+            line = scan.nextLine();
+            String[] words = line.toLowerCase().split(" ");
+            for (String word : words) {
+                if (!word.startsWith(LINK_PREFIX)) {
+                    continue;
+                }
+                String link = word.substring(LINK_PREFIX.length());
+                Link createdLink = createLink(link);
+                if (createdLink == null) {
+                    continue;
+                }
+                this.link.add(createdLink);
+//                System.out.println(link.toLowerCase());
+            }
+        }
+    }
+
+    public static boolean isCorrectId(String id) {
+        return LINK_ID_PATTERN.matcher(id).matches();
+    }
+
+    // accepts only small letters, capital letters, digits and '_' (except the first character)
+    @SuppressWarnings("DuplicatedCode")
+    static Link createLink(String link) {
+        Matcher matcher = LINK_PATTERN.matcher(link);
+        if (!matcher.matches()) {
+            return null;
+        }
+        String ref = matcher.group(1);
+        String idString = matcher.group(2);
+        int weight = idString == null ? 1 : Integer.parseInt(idString);
+        return weight == 0 ? null : new Link(ref, weight);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("Document: ").append(name);
+        Iterator<Link> iter = link.iterator();
+        while (iter.hasNext()) {
+            StringBuilder subResult = new StringBuilder("\n");
+            for (int i = 0; i < 9; i++) {
+                subResult.append(iter.next());
+                if (!iter.hasNext()) {
+                    break;
+                }
+                subResult.append(" ");
+            }
+            if (iter.hasNext()) {
+                subResult.append(iter.next());
+            }
+            result.append(subResult);
+        }
+        return result.toString();
+    }
+
+    public String toStringReverse() {
+        StringBuilder result = new StringBuilder("Document: ").append(name);
+        ListIterator<Link> iter = link.listIterator();
+        while (iter.hasNext()) iter.next();
+        while (iter.hasPrevious()) {
+            StringBuilder subResult = new StringBuilder("\n");
+            for (int i = 0; i < 9; i++) {
+                subResult.append(iter.previous());
+                if (!iter.hasPrevious()) {
+                    break;
+                }
+                subResult.append(" ");
+            }
+            if (iter.hasPrevious()) {
+                subResult.append(iter.previous());
+            }
+            result.append(subResult);
+        }
+        return result.toString();
+    }
+
+    public int[] getWeights() {
+        //TODO
+        return null;
+    }
+
+    public static void showArray(int[] arr) {
+        // TODO
+    }
+
+    void bubbleSort(int[] arr) {
+        showArray(arr);
+        //TODO
+    }
+
+    public void insertSort(int[] arr) {
+        showArray(arr);
+        //TODO
+
+    }
+
+    public void selectSort(int[] arr) {
+        showArray(arr);
+        //TODO
+    }
+
+}
---
+++ aisd/dsaa.lab05/Link.java
@@ -0,0 +1,39 @@
+package dsaa.lab05;
+
+public class Link implements Comparable<Link> {
+    public String ref;
+    public int weight;
+
+    public Link(String ref) {
+        this(ref, 1);
+    }
+
+    public Link(String ref, int weight) {
+        this.ref = ref.toLowerCase();
+        this.weight = weight;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Link)) {
+            return false;
+        }
+//        Cannot use pattern variable because of compatibility with Java 8
+        @SuppressWarnings("PatternVariableCanBeUsed") Link link = (Link) other;
+        return ref.equals(link.ref);
+    }
+
+    @Override
+    public String toString() {
+        return ref + "(" + weight + ")";
+    }
+
+    @Override
+    public int compareTo(Link another) {
+        return ref.compareTo(another.ref);
+    }
+}
+
