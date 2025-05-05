2b921c549c40486cf823c09e0e2119e7d464cd7f
Konrad Guzek <konrad@guzek.uk>
2025-05-05 18:41:11 +0200

    ak(ex01): handle div by 0

--- ak/ex01/mips1.asm
+++ ak/ex01/mips1.asm
@@ -4,6 +4,7 @@
 	input_operator: .asciiz "Input operator (0+, 1-, 2/, 3*): "
 	invalid_operator: .asciiz "Invalid operator"
 	input_loop: .asciiz "\nCalculate again? (0/1): "
+	divide_by_zero: .asciiz "Cannot divide by 0"
 .text
 main:
 	# first operand
@@ -28,11 +29,11 @@ main:
 	syscall
 	move $t1, $v0
 	# switch
-	beq, $t2, 0, addition
-	beq, $t2, 1, subtraction
-	beq, $t2, 2, division
-	beq, $t2, 3, multiplication
-	j error
+	beq $t2, 0, addition
+	beq $t2, 1, subtraction
+	beq $t2, 2, division
+	beq $t2, 3, multiplication
+	j error_operator
 addition:
 	add $t3, $t0, $t1
 	j end
@@ -40,24 +41,31 @@ subtraction:
 	sub $t3, $t0, $t1
 	j end
 division:
-	div $t3, $t0, $t1
+	beq $t1, 0, error_division
+	div $t0, $t1
+	mflo $t3
 	j end
 multiplication:
 	mul $t3, $t0, $t1
 	j end
-error:
+error_division:
+	li $v0, 4
+	la $a0, divide_by_zero
+	syscall
+	j ask_loop
+error_operator:
 	li $v0, 4
 	la $a0, invalid_operator
 	syscall
-	j EOF
+	j ask_loop
 end:
 	li $v0, 1
 	move $a0, $t3
 	syscall
+ask_loop:
 	li $v0, 4
 	la $a0, input_loop
 	syscall
 	li $v0, 5
 	syscall
-	beq, $v0, 1, main
-EOF:
+	beq $v0, 1, main
