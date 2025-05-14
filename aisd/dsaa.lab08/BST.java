d4cdd701e488627033b0ae17c8c786ddceef8fef
Konrad Guzek <konrad@guzek.uk>
2025-05-14 20:53:06 +0200

    dsaa(lab8): implement BST

--- aisd/dsaa.lab08/BST.java
+++ aisd/dsaa.lab08/BST.java
@@ -1,53 +1,204 @@
 package dsaa.lab08;
 
-public class BST<T> {
-    private final Node root = null;
+import java.util.NoSuchElementException;
+
+public class BST<T extends Comparable<T>> {
+    private Node root = null;
+    private int size = 0;
 
     public BST() {
     }
 
     public T getElement(T toFind) {
-        // TODO
-        return null;
+        Node curr = root;
+
+        while (curr != null) {
+            int cmp = toFind.compareTo(curr.value);
+
+            if (cmp < 0) {
+                curr = curr.left;
+            } else if (cmp > 0) {
+                curr = curr.right;
+            } else {
+                return curr.value;
+            }
+        }
+
+        throw new NoSuchElementException();
     }
 
     public T successor(T elem) {
-        // TODO
-        return null;
-    }
+        Node node = findNode(elem);
+        if (node == null) throw new NoSuchElementException();
 
-    public String toStringInOrder() {
-        // TODO
-        return null;
-    }
+        if (node.right != null) {
+            Node next = node.right;
+            while (next.left != null) next = next.left;
+            return next.value;
+        } else {
+            Node parent = node.parent;
+            Node current = node;
+            while (parent != null && current == parent.right) {
+                current = parent;
+                parent = parent.parent;
+            }
+            return parent != null ? parent.value : null;
+        }
 
-    public String toStringPreOrder() {
-        // TODO
-        return null;
-    }
-
-    public String toStringPostOrder() {
-        // TODO
-        return null;
     }
 
     public boolean add(T elem) {
-        // TODO
-        return false;
+        if (root == null) {
+            root = new Node(elem);
+            size++;
+            return true;
+        }
+
+        Node parent = null;
+        Node current = root;
+
+        while (current != null) {
+            parent = current;
+            if (elem.compareTo(current.value) < 0) {
+                current = current.left;
+            } else if (elem.compareTo(current.value) > 0) {
+                current = current.right;
+            } else {
+                return false;
+            }
+        }
+
+        Node newNode = new Node(elem);
+        newNode.parent = parent;
+
+        if (elem.compareTo(parent.value) < 0) parent.left = newNode;
+        else parent.right = newNode;
+        size++;
+        return true;
+
     }
 
     public T remove(T value) {
-        // TODO
+        Node node = findNode(value);
+        if (node == null) return null;
+
+        T oldValue = node.value;
+
+        if (node.left != null && node.right != null) {
+            Node successor = findMinimalNode(node.right);
+            node.value = successor.value;
+            node = successor;
+        }
+
+        Node pullUp = node.left != null ? node.left : node.right;
+
+        if (pullUp != null) {
+            pullUp.parent = node.parent;
+            if (node.parent == null) root = pullUp;
+            else if (node == node.parent.left) node.parent.left = pullUp;
+            else node.parent.right = pullUp;
+        } else if (node.parent == null) {
+            root = null;
+        } else {
+            if (node == node.parent.left) node.parent.left = null;
+            else node.parent.right = null;
+        }
+
+        size--;
+        return oldValue;
+    }
+
+    private Node findMinimalNode(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+    }
+
+    private Node findNode(T value) {
+        Node current = root;
+
+        while (current != null) {
+            int compareValue = value.compareTo(current.value);
+            if (compareValue == 0) {
+                return current;
+            } else if (compareValue < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
         return null;
     }
 
     public void clear() {
-        // TODO
+        size = 0;
+        root = null;
     }
 
     public int size() {
-        // TODO
-        return 0;
+        return size;
+    }
+
+    public String toStringInOrder() {
+        StringBuilder sb = new StringBuilder();
+        inOrder(root, sb);
+        if (sb.isEmpty()) return " ";
+        return sb.toString().trim().substring(0, sb.length() - 2);
+    }
+
+    public String toStringPreOrder() {
+        StringBuilder sb = new StringBuilder();
+
+        preOrder(root, sb);
+
+        if (sb.isEmpty()) {
+            return " ";
+        }
+
+        return sb.toString().trim().substring(0, sb.length() - 2);
+    }
+
+    public String toStringPostOrder() {
+        StringBuilder sb = new StringBuilder();
+
+        postOrder(root, sb);
+
+        if (sb.isEmpty()) {
+            return " ";
+        }
+
+        return sb.toString().trim().substring(0, sb.length() - 2);
+    }
+
+    private void inOrder(Node node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, sb);
+        sb.append(node.value).append(", ");
+        inOrder(node.right, sb);
+    }
+
+    private void preOrder(Node node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        sb.append(node.value).append(", ");
+        preOrder(node.left, sb);
+        preOrder(node.right, sb);
+    }
+
+    private void postOrder(Node node, StringBuilder sb) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left, sb);
+        postOrder(node.right, sb);
+        sb.append(node.value).append(", ");
     }
 
     private class Node {
@@ -57,14 +208,7 @@ public class BST<T> {
         public Node(T v) {
             value = v;
         }
-
-        public Node(T value, Node left, Node right, Node parent) {
-            super();
-            this.value = value;
-            this.left = left;
-            this.right = right;
-            this.parent = parent;
-        }
     }
+
 
 }
