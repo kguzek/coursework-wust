c46360f25703b8978f4b86a5a963d646a410c53a
Konrad Guzek <konrad@guzek.uk>
2025-05-27 16:20:57 +0200

    ak(lab4): progress

--- ak/ex04/stack.asm
+++ ak/ex04/stack.asm
@@ -1,6 +1,7 @@
 .data
 	prompt_input: .asciiz "Input number of strings... "
-
+	input_buffer: .space 199
+	input_size: .byte 200
 .text
 main:
 	li $v0, 4
@@ -9,4 +10,12 @@ main:
 	li $v0, 5
 	syscall
 	move $t0, $v0
-	
+loop:
+	beqz $t0, loop_end
+	li $v0, 8
+	la $a0, input_buffer
+	la $a1, input_size
+	syscall
+	sub $t0, $t0, 1
+	j loop
+loop_end:
