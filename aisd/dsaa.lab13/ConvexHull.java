56d7ed9e09a264dce21d08a9131e1458746b37c2
Konrad Guzek <konrad@guzek.uk>
2025-06-17 21:19:22 +0200

    dsaa(lab13): init

---
+++ aisd/dsaa.lab13/ConvexHull.java
@@ -0,0 +1,12 @@
+package dsaa.lab13;
+
+import java.util.LinkedList;
+
+public class ConvexHull {
+
+    public static LinkedList<Point> solve(LinkedList<Point> points) {
+        //TODO
+        return null;
+    }
+
+}
