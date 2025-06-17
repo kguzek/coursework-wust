220ce05f6ca5bd2c522754d875553357036041b4
Konrad Guzek <konrad@guzek.uk>
2025-06-17 15:36:54 +0200

    sysop(lab5): print load averages

--- sysop/lab5/main.py
+++ sysop/lab5/main.py
@@ -2,6 +2,7 @@ from lab5.algorithms.algorithm_1 import Algorithm1
 from lab5.algorithms.algorithm_2 import Algorithm2
 from lab5.algorithms.algorithm_3 import Algorithm3
 from lab5.generation import generate_processor_count, generate_processor
+from lab5.snapshot import SchedulerSnapshot
 from lab5.visualizer import display_results
 
 
@@ -28,7 +29,17 @@ def main():
     for algorithm in algorithms:
         print("Running", algorithm, "...")
         algorithm.run()
-        _, stats = algorithm.statistics[-1]
+        total_data_points = len(algorithm.statistics)
+        average_load = 0
+        average_deviation = 0
+        stats: SchedulerSnapshot | None = None
+        for _, stats in algorithm.statistics:
+            average_load += stats.load_average
+            average_deviation += stats.load_standard_deviation
+        average_load /= total_data_points
+        average_deviation /= total_data_points
+        print(f"Average load: {average_load:.2f}%")
+        print(f"Average standard deviation: {average_deviation:.2f} percentage points")
         print("Total load queries:", stats.total_load_queries)
         print("Total delegated processes:", stats.total_delegated_processes)
         print()
