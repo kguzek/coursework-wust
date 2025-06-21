7133d4683e71f57f363a0dcee622bb158bd530cb
Konrad Guzek <konrad@guzek.uk>
2025-06-21 16:31:45 +0200

    dsaa(lab12): implement KMP, add tests

--- aisd/dsaa.lab12/Automaton.java
+++ aisd/dsaa.lab12/Automaton.java
@@ -7,24 +7,26 @@ public class Automaton implements IStringMatcher {
     @Override
     public LinkedList<Integer> validShifts(String pattern, String text) {
         // TODO: Implement automaton
-        final int patternLength = text.length();
-        final int textLength = pattern.length();
-        final int maxShift = patternLength - textLength + 1;
+        final int patternLength = pattern.length();
         LinkedList<Integer> shifts = new LinkedList<>();
+        if (patternLength == 0) {
+            return shifts;
+        }
+        final int textLength = text.length();
+        final int maxShift = textLength - patternLength + 1;
         for (int i = 0; i < maxShift; i++) {
             int j = 0;
-            while (j < textLength) {
+            while (j < patternLength) {
                 if (text.charAt(i + j) == pattern.charAt(j)) {
                     j++;
                 } else {
                     break;
                 }
             }
-            if (j == textLength) {
+            if (j == patternLength) {
                 shifts.add(i);
             }
         }
         return shifts;
     }
-
 }
--- aisd/dsaa.lab12/KMP.java
+++ aisd/dsaa.lab12/KMP.java
@@ -4,10 +4,48 @@ import java.util.LinkedList;
 
 public class KMP implements IStringMatcher {
 
-    @Override
-    public LinkedList<Integer> validShifts(String pattern, String text) {
-        // TODO: Implement KMP
-        return new Automaton().validShifts(pattern, text);
+    private int[] generatePiArray(String pattern) {
+        final int patternLength = pattern.length();
+        int[] pi = new int[patternLength];
+        int j = 0;
+        for (int i = 1; i < patternLength; i++) {
+            char current = pattern.charAt(i);
+            while (j > 0 && current != pattern.charAt(j)) {
+                j = pi[j - 1];
+            }
+            if (current == pattern.charAt(j)) {
+                j++;
+            }
+            pi[i] = j;
+        }
+        return pi;
     }
 
+    @Override
+    public LinkedList<Integer> validShifts(String pattern, String text) {
+        final int[] pi = generatePiArray(pattern);
+        final int patternLength = pattern.length();
+        final int textLength = text.length();
+        final LinkedList<Integer> shifts = new LinkedList<>();
+        if (patternLength == 0) {
+            return shifts;
+        }
+        int i = 0;
+        int j = 0;
+        while (i < textLength) {
+            if (pattern.charAt(j) == text.charAt(i)) {
+                j++;
+                i++;
+            } else if (j == 0) {
+                i++;
+            } else {
+                j = pi[j - 1];
+            }
+            if (j == patternLength) {
+                shifts.addLast(i - patternLength);
+                j = pi[j - 1];
+            }
+        }
+        return shifts;
+    }
 }
