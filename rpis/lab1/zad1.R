1d6466cdd2d09a479cfcd87923c315e384220118
Konrad Guzek <konrad@guzek.uk>
2025-10-08 18:06:13 +0200

    rpis(lab1): format zad1

--- rpis/lab1/zad1.R
+++ rpis/lab1/zad1.R
@@ -1,14 +1,14 @@
-a = c(1, 4, 6, 13, -10, 8)
-b = seq(1,101,2)
-c = c(rep(4, 3), rep(7, 3), rep(9, 3))
-d = c("czy","to","jest","wektor",NA)
-e = c("czy","to","jest","wektor","NA")
-f = c(rep(c(4, 7, 9), 6))
+a <- c(1, 4, 6, 13, -10, 8)
+b <- seq(1, 101, 2)
+c <- c(rep(4, 3), rep(7, 3), rep(9, 3))
+d <- c("czy", "to", "jest", "wektor", NA)
+e <- c("czy", "to", "jest", "wektor", "NA")
+f <- c(rep(c(4, 7, 9), 6))
 
-vectors = list(a,b,c,d,e,f)
+vectors <- list(a, b, c, d, e, f)
 
 for (i in seq_along(vectors)) {
-  v = vectors[[i]]
+  v <- vectors[[i]]
   print(paste("długość", i, ":", length(v)))
   print(paste("typ", i, ":", typeof(v)))
   print(paste("min.", i, ":", min(v)))
@@ -16,15 +16,15 @@ for (i in seq_along(vectors)) {
   if (typeof(v) != "character") {
     print(paste("suma", i, ":", sum(v)))
   }
-  cat('\n')
+  cat("\n")
 }
 
 print(sort(d))
 print(sort(e))
-print(a+f)
-print(a*f)
-print(a+c)
-print(a+10)
+print(a + f)
+print(a * f)
+print(a + c)
+print(a + 10)
 print(b[26])
 print(f[6:10])
-print(b[b>50])
+print(b[b > 50])
